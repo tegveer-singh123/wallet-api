2b3172c046d884184fd6eb807e69f45eb38593fc
Tegveer Singh <tegisingh94@gmail.com>
2025-05-01 13:10:51 +0530

    initial project setup with config, database connection, and Docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tegveer-singh123/wallet-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Im in")
+	config.LoadEnv()
+	config.ConnectDatabase()
+
+	port := os.Getenv("APP_PORT")
+
+	r := gin.Default()
+
+	// Health check endpoint
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Wallet API is running"})
+	})
+
+	err := r.Run(":"+port)
+	if err != nil {
+		log.Fatal("Failed to run server:", err)
+	}
 }
